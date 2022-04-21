80cb8e604845d7ac1247b46ea0008f7959a0bca8
command0 <c0d3r.command0@gmail.com>
2022-04-21 11:36:11 +0530

    commit on main

---
+++ learning js/js/7.manipulating websites.js
@@ -0,0 +1,8 @@
+// console.log(`manipulatind websites`);
+
+// let a = window.document;
+// a = document.images
+// a = document.scripts[0].src
+// console.log(a);
+
+
--- learning js/js/manipulating websites.js
+++
@@ -1,8 +0,0 @@
-// console.log(`manipulatind websites`);
-
-// let a = window.document;
-// a = document.images
-// a = document.scripts[0].src
-// console.log(a);
-
-
